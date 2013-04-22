bd45927a8e48f8e60cf75cc378c951f11a6eeb8e
Roy Clarkson <rclarkson@vmware.com>
2013-04-22 16:37:22 -0400

    Add initial 'complete' code

---
+++ complete/src/org/hello/HelloActivity.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hello;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		String searchUrl = "http://search.twitter.com/search.json?q={query}";
+
+		String queryParameter = "@gopivotal";
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+		String responseBody = restTemplate.getForObject(searchUrl, String.class, queryParameter);
+		
+		Log.d("Consuming REST", responseBody);
+	}
+
+}
