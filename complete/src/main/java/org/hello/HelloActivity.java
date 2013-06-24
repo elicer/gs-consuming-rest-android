33b01305fa830a3fc4f08aeb980d1aed52bd49a5
Roy Clarkson <rclarkson@gopivotal.com>
2013-06-24 00:27:22 -0400

    Update sample code to request data from Facebook

--- complete/src/main/java/org/hello/HelloActivity.java
+++ complete/src/main/java/org/hello/HelloActivity.java
@@ -1,10 +1,12 @@
 package org.hello;
 
+import org.hello.R;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloActivity extends Activity {
 
@@ -12,14 +14,17 @@ public class HelloActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_layout);
+    }
 
-        String url = "http://search.twitter.com/search.json?q={query}";
-
-        String queryParameter = "@gopivotal";
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        final String url = "http://search.twitter.com/search.json?q={query}";
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        TwitterSearchResults results = restTemplate.getForObject(url, TwitterSearchResults.class, queryParameter);
+        Page page = restTemplate.getForObject("http://graph.facebook.com/gopivotal", Page.class);
+        TextView textView = (TextView) this.findViewById(R.id.text_view);
+        textView.setText(page.getName());
     }
 
 }
