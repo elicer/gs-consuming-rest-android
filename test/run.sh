5058717539cc3396a8be59557147bf031e6a27c2
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-07 18:19:56 -0400

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 cd $(dirname $0)
+
 cd ../complete
 mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
 rm -rf target
+
 cd ../initial
 mvn clean package
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
 rm -rf target
+
 exit $ret
